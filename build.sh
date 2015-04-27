be26bd08628d94934442954083299be9ac67055a
Manfred Touron <m@42.am>
2015-04-27 22:26:21 +0200

    Updated build.sh

--- build.sh
+++ build.sh
@@ -8,8 +8,11 @@ ARCH=$(uname -m)
 case "$ARCH" in
     x86_64)
         mkdir -p "${ARCH}"
-        docker run -it --rm ubuntu:latest /bin/bash -xec "apt-get update; apt-get install qemu-user-static"
+        docker run -it ubuntu:latest /bin/bash -xec "apt-get update; apt-get install -y -qq qemu-user-static"
         docker cp $(docker ps -lq):/usr/bin/ "${ARCH}"/
+	docker rm $(docker ps -lq)
+	cp -rf "${ARCH}"/bin/qemu-*-static "${ARCH}"/
+	rm -rf "${ARCH}"/bin/
         ;;
     *)
         echo "Invalid arch: $ARCH"
